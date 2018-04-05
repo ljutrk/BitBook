830c574cc4c853fb5b310816e8e9bbf5751929a3
Bit Student <BitStudent@bgit.rs>
2018-04-05 11:27:08 +0200

    add loader on feedpage

---
+++ src/app/partials/Loader.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./loader.css";
+
+const Loader = () => {
+    return (
+        <div class="spinner">
+            <div class="cube1"></div>
+            <div class="cube2"></div>
+        </div>
+    )
+}
+export { Loader };
